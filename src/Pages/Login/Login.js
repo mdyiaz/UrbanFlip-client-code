8fa72f7fa9484bdb2a3e4eca6f41521ca0ec1a70
mdyiaz <mdyiaz@gmail.com>
2022-11-08 16:12:34 +0600

    login section added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -19,19 +19,19 @@ const Login = () => {
 
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <h1 className="text-5xl font-bold text-center">Login now!</h1>
+          <h1 className="text-5xl font-bold text-center">Login now !</h1>
             <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input type="email" placeholder="email" name='email' className="input input-bordered" />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input type="password" placeholder="password" name='password' className="input input-bordered" />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -19,25 +19,31 @@ const Register = () => {
 
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <h1 className="text-5xl font-bold text-center">Register!</h1>
+          <h1 className="text-5xl font-bold text-center">Register !</h1>
             <form onSubmit={handleSignUp} className="card-body">
+
+            <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name='name' placeholder="Enter your name" required className="input input-bordered" />
+              </div>
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input type="email" name='email' placeholder="email" required className="input input-bordered" />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
+                <input type="password" name='password' placeholder="password" required className="input input-bordered" />
+                
               </div>
               <div className="form-control mt-6">
-                <input className="btn btn-primary" type="submit" value="Login Now !" />
+                <input className="btn btn-primary" type="submit" value="Register Now !" />
 
                 <p className='mt-3 text-sm text-center'>Already have an Account ?<Link to ="/login" className='text-orange-600 font-semibold'>Login here</Link></p>
                 
