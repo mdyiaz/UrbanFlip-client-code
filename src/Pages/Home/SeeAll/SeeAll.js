a3a21b9070b483d64a8a1fed92118db75d5daa8a
mdyiaz <mdyiaz@gmail.com>
2022-11-08 12:35:19 +0600

    added see all

--- src/Pages/Home/SeeAll/SeeAll.js
+++ src/Pages/Home/SeeAll/SeeAll.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const SeeAll = () => {
     return (
-        <div>
-            
+        <div className='text-center mb-10 mx-20 mt-10 shadow-xl'>
+            <button className="btn btn-accent w-full">See All Services</button>
+
         </div>
     );
 };
